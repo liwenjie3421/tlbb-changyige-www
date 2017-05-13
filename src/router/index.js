41788f6a1e1b372288e7a0a2cbf065d2d7ccc3b7
liwenjie <wjli@wisedu.com>
2017-05-13 15:55:10 +0800

    router

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 
 import Index from '../pages/Index.vue';
+import SearchConfig from '../pages/SearchConfig.vue';
 
 Vue.use(Router);
 
@@ -11,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'Index',
       component: Index
+    },
+    {
+      path: '/SearchConfig',
+      name: 'SearchConfig',
+      component: SearchConfig
     }
   ]
 });
