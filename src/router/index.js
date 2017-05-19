0b88c7e2254a02c86ab76f1732b74ea34b8dda89
liwenjie <wjli@wisedu.com>
2017-05-19 13:11:40 +0800

    add router home

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 
 import Index from '../pages/Index.vue';
+import Home from '../pages/Home.vue';
 import SearchConfig from '../pages/SearchConfig.vue';
 
 Vue.use(Router);
@@ -10,6 +11,11 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/index',
       name: 'Index',
       component: Index
     },
