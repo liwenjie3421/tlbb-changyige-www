ea76b154a77a48e41df4fc2399cc12525846308a
liwenjie <wjli@wisedu.com>
2017-06-01 18:51:58 +0800

    stateArea routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,6 +1,7 @@
 import SearchConfig from '../pages/SearchConfig.vue';
 import List from '../pages/List.vue';
 import User from '../pages/User.vue';
+import StateArea from '../pages/StateArea.vue';
 
 export default [
     {
@@ -17,5 +18,10 @@ export default [
         path: '/user',
         name: 'user',
         component: User
+    },
+    {
+        path: '/stateArea',
+        name: 'stateArea',
+        component: StateArea
     }
 ];
