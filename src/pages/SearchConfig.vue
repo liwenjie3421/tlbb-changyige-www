1a926938b296719bf647c3f5cb401fe0fe921a61
liwenjie <wjli@wisedu.com>
2017-06-01 17:46:29 +0800

    配置条件页

--- src/pages/SearchConfig.vue
+++ src/pages/SearchConfig.vue
@@ -1,19 +1,28 @@
 <template>
   <div id="searchConfig">
+    <m-header title="条件配置"></m-header>
+    <m-cell title="搜索条件配置" is-link></m-cell>
+    <m-cell title="选择公示或售卖" is-link></m-cell>
+    <m-cell title="选择区服" is-link></m-cell>
     <m-tabs></m-tabs>
   </div>
 </template>
 <script>
+  import {
+    Cell,
+    Header
+  } from 'mint-ui';
   import mTabs from '../components/Tabs.vue';
+
   export default {
     name: 'searchConfig',
     components: {
-      mTabs
+      mTabs,
+      mHeader: Header,
+      mCell: Cell
     },
     data() {
-      return {
-        selected: ''
-      }
+      return {}
     }
   };
 
