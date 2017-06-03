fb5b0d3827e94ea13f7e760436de7b6e56aec130
liwenjie <wjli@wisedu.com>
2017-06-03 16:06:13 +0800

    servers 搜索

--- src/pages/ServersGroup.vue
+++ src/pages/ServersGroup.vue
@@ -2,7 +2,7 @@
   <div id="searchGroup">
     <m-search v-model="searchServerCondition" :autofocus="true">
       <div v-for="item in searchServerResult" @click="test(item)">
-        <m-cell :title="item.label" :key="item.world_id" :value="item.area_name">
+        <m-cell :title="item.world_name" :key="item.world_id" :value="item.area_name">
         </m-cell>
       </div>
     </m-search>
@@ -23,29 +23,51 @@
     data() {
       return {
         searchServerCondition: '',
-        searchServerResult: [{
-            label: 'a',
-            world_id: 'world_id_a',
-            area_name: 'area_name_a'
-          },
-          {
-            label: 'b',
-            world_id: 'world_id_b',
-            area_name: 'area_name_b'
-          }
-        ]
+        searchServerResult: [],
+        serversGroup: {}
       }
     },
+    created() {
+      this.$http.post('/api/getServers').then((r) => {
+        let serversObject = {};
+        let serversName = [];
+        r.data.result.map(area => {
+          area.server.map(server => {
+            serversObject[server.name] = {
+              area_name: area.name,
+              world_name: server.name,
+              world_id: server.id
+            };
+            serversName.push(server.name);
+          });
+        });
+        this.serversGroup.servers = serversObject;
+        this.serversGroup.serversName = serversName;
+      });
+    },
     methods: {
-        test(value) {
-            console.log(value);
-        }
+      test(value) {
+        console.log(value);
+      }
     },
     watch: {
-      searchServerCondition(v) {
-        this.$http.post('/api/getServers').then((r)=>{
-          console.log(r);
-        });
+      searchServerCondition(value) {
+        clearTimeout(this.ssTimeOut);
+        this.searchServerResult = [];
+        this.ssTimeOut = setTimeout(() => {
+          let r = [];
+          this.serversGroup.serversName.map((v, k) => {
+            if (v.indexOf(value) > -1) {
+              let servers = this.serversGroup.servers;
+              r.push({
+                area_name: servers[v].area_name,
+                world_name: servers[v].world_name,
+                world_id: servers[v].world_id
+              });
+            }
+          });
+          this.searchServerResult = r;
+        }, 200);
       }
     }
   };
