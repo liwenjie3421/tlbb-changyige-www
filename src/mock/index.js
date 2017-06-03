48da690cbf119ccad21272e7022d52ab46430164
liwenjie <wjli@wisedu.com>
2017-06-03 15:49:40 +0800

    丢给前端做

--- src/mock/index.js
+++ src/mock/index.js
@@ -3,19 +3,7 @@ import Mock from 'mockjs';
 import serversInfo from './serversInfo';
 const mockConfig = {
     'getServers': {
-        result: (() => {
-            let serversObject = {};
-            serversInfo.map(serversGroup => {
-                serversGroup.server.map(server => {
-                    serversObject[server.name] = {
-                        area_name: serversGroup.name,
-                        world_name: server.name,
-                        world_id: server.id
-                    }
-                });
-            });
-            return serversObject;
-        })()
+        result: serversInfo
     }
 };
 
