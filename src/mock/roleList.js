368d4d930f5bba97955220983865f0b74083915e
liwenjie <wjli@wisedu.com>
2017-06-08 14:48:40 +0800

    mock rolelist

--- src/mock/roleList.js
+++ src/mock/roleList.js
@@ -1,7 +1,7 @@
 var data = [{
     roleName: /[a-z][A-Z][0-9]/,
     roleRef:'@url',
-    roleProfession:  /\w/,
-    roleLevel: /\d/
+    roleProfession:  /少林|明教|丐帮|武当|峨嵋|星宿|天龙|天山|逍遥|慕容|唐门|鬼谷/,
+    roleLevel: /\d\d/
 }];
 export default data;
