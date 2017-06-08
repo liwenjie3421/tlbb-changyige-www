e1be12eaf55f37a0e26b7cd40a5b1ad17179121a
liwenjie <wjli@wisedu.com>
2017-06-08 14:49:04 +0800

    rolelist

--- src/pages/list/List.vue
+++ src/pages/list/List.vue
@@ -1,14 +1,33 @@
 <template>
   <div id="list">
+    <div v-for="role in roleList">
+      <m-cell :title="`${role.roleName} [${role.roleProfession}] ${role.roleLevel}级`"></m-cell>
+    </div>
     <m-tabs></m-tabs>
   </div>
 </template>
 <script>
+  import {
+    Cell
+  } from 'mint-ui';
   import mTabs from '../../components/Tabs.vue';
+
   export default {
     name: 'list',
     components: {
+      mCell: Cell,
       mTabs
+    },
+    data() {
+      return {
+        roleList: []
+      }
+    },
+    created() {
+      this.$http.post('api/getRoleList', {}).then((d) => {
+        console.log(d.data.result);
+        this.roleList = d.data.result;
+      });
     }
   }
 
