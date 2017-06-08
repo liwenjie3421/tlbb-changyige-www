9b37e289139642b6a7ca636741a0fe13bbafe5a6
liwenjie <wjli@wisedu.com>
2017-06-08 14:52:05 +0800

    orderby pages

---
+++ src/pages/search/OrderBy.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="orderBy">
+    别急，在写
+  </div>
+</template>
+<script>
+  import {
+    Cell,
+    Header,
+    Button
+  } from 'mint-ui';
+
+  export default {
+    name: 'orderBy',
+    components: {
+      mHeader: Header,
+      mCell: Cell,
+      mButton: Button
+    },
+    data() {
+      return {
+
+      }
+    },
+    created() {
+    },
+    methods: {
+    }
+  };
+
+</script>
+<style>
+
+
+</style>
